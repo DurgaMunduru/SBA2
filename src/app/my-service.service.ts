7e8a0b13837265ad8be32b1da23ddb85cbbb0e3f
Durgaprasad <munduru.durgaprasad@dxc.com>
2023-07-04 15:34:46 +0530

    CustomerApp

---
+++ src/app/my-service.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyServiceService {
+
+
+  toDo(message:string){
+    console.log(message);
+  }
+
+  constructor() { }
+}
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MyServiceService } from '../my-service.service';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+
+
+
+  constructor(private myService:MyServiceService){
+
+  }
+  
+   
+  callService(){
+    this.myService.toDo("Hello , This is customer1");
+  }
+
+ 
+
+}
